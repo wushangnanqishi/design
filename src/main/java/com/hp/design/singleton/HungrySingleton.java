97054199a15dcf8c458d9c6a947393b18ffe1ebe
huangpeng07 <huangpeng07@58ganji.com>
2019-03-13 18:34:16 +0800

    饥汉式单例

--- src/main/java/com/hp/design/singleton/HungrySingleton.java
+++ src/main/java/com/hp/design/singleton/HungrySingleton.java
@@ -1,7 +1,22 @@
 package com.hp.design.singleton;
 
 /**
- * Created by huangpeng07 on 2019/3/13.
+ * 饿汉式单例模式
+ * JVM 加载该类时，即生成该类的实例
+ * 优点：不会因为多线程环境下，生成多个HungrySingleton对象
+ * 缺点： 无论程序需不需要HungrySingleton对象，程序只要加载了HungrySingleton，
+ * 就有一个HungrySingleton对象
  */
 public class HungrySingleton {
+
+    private static HungrySingleton  hungrySingleton = new HungrySingleton();
+
+    private HungrySingleton(){
+
+    }
+
+    public static HungrySingleton getInstance(){
+        return hungrySingleton;
+    }
+
 }
